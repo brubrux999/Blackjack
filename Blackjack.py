deffa9cc1637a2d24844420be8e8b86e762b31ba
brubrux999 <brunylarrez@gmail.com>
2026-01-15 02:39:36 -0600

    Hands as lists

--- Blackjack.py
+++ Blackjack.py
@@ -7,26 +7,29 @@ deck = [
     "As", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 ]
 
-def dealing_cards(deck):
+player_hand = []
+dealer_hand = []
+
+def dealing_cards():
     shuffle(deck)
 
-    player_hand = deck[:2] # Take the first two cards
-    deck = deck[2:] # Remove those cards taken from deck
+    player_hand.extend(deck[0:2]) # Take the first two cards
+    del deck[0:2] # Remove those cards taken from deck
 
-    dealer_hand = deck[:2]
-    deck = deck[2:]
+    dealer_hand.extend(deck[0:2])
+    del deck[0:2]
 
     player_score = hand_value(player_hand) # Compute the hand value
     dealer_score = hand_value(dealer_hand)
 
-    return player_hand, player_score, dealer_hand, dealer_score, deck
+    return player_score, dealer_score
 
-def take_a_card(hand, score, deck):
+def take_a_card(hand, score):
     hand.append(deck[0]) # Add a card to (player/dealer) hand
     deck.pop(0) # Remove that card from deck
     score = hand_value(hand)
 
-    return hand, score, deck
+    return score
 
 def hand_value(hand):
     As = 0 # Card counter "As"
@@ -60,14 +63,11 @@ def compare_scores(player_score, dealer_score):
     else:
         return print("It's a push")
 
-def play(deck):
+def play():
     blackjack = False
     print("\nThe dealer deals the cards...\n")
-    player_hand, player_score, dealer_hand, dealer_score, deck = dealing_cards(deck)
-    print(
-        f">> Dealer hand: [{dealer_hand[0]}, ?], the score is {hand_value([dealer_hand[0]])}\n"
-        f">> Your hand: {player_hand}, the score is {player_score}\n"
-    )
+    player_score, dealer_score = dealing_cards()
+    show_hands(player_hand, player_score, [dealer_hand[0], "?"], hand_value([dealer_hand[0]]))
     
     # Evaluate if there is blackjack
     if dealer_score == 21:
@@ -88,16 +88,17 @@ def play(deck):
 
         if hit_stand == "h":
             print("The player hits")
-            player_hand, player_score, deck = take_a_card(player_hand, player_score, deck)
-            show_hands(player_hand, player_score, [dealer_hand[0], "?"], dealer_score)
+            player_score = take_a_card(player_hand, player_score)
+            show_hands(player_hand, player_score, [dealer_hand[0], "?"], hand_value([dealer_hand[0]]))
             if player_score > 21:
                 return print("The player busts\n")
-        elif hit_stand == "s": # Dealer turn
+        # Dealer turn
+        elif hit_stand == "s":
             print("Dealer reveals the hole card...")
             show_hands(player_hand, player_score, dealer_hand, dealer_score)
             while dealer_score < 17:
                 print("The dealer draws a card")
-                dealer_hand, dealer_score, deck = take_a_card(dealer_hand, dealer_score, deck)
+                dealer_score = take_a_card(dealer_hand, dealer_score)
                 show_hands(player_hand, player_score, dealer_hand, dealer_score)
                 if dealer_score > 21:
                     return print("The dealer busts\n")
@@ -124,18 +125,24 @@ while option != 1 or option != 2 or option != 3:
     option = int(input("Select an option: "))
     if option == 1:
         print("\nGood luck!")
-        play(deck)
+        play()
         play_again = "y"
         while play_again != "n":
             play_again = input("Do you wanna play again(y/n)? ")
             if play_again == "y":
-                play(deck)
+                # Reset hands and deck
+                player_hand.clear()
+                dealer_hand.clear()
+                play()
             elif play_again == "n":
+                print("Thanks for playing!!\n")
                 menu()
                 break
             else:
                 print("Invalid input!\n")
     elif option == 2:
         break
+    elif option == 3:
+        print("Seccion de prueba")
     else:
         print("Invalid input!\n")
