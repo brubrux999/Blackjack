a8ad9dcb845932641f308756adb081ea269ca6ac
Bruno Alarcón <brunylarrez@gmail.com>
2026-01-20 02:23:05 -0600

    v2.0 Structure in classes
    * player class
    
    * readme update

--- Blackjack.py
+++ Blackjack.py
@@ -1,100 +1,91 @@
-from ast import Global
 from random import shuffle
 
-# Hands to display
-player_hand = []
-dealer_hand = []
-# Hands to compute values (without str values)
-phand = []
-dhand = []
-# Scores
-player_score = 0
-dealer_score = 0
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+        self.num_hand = []
+        self.score = 0
+        self.wins = 0
 
-# Victory counter
-player_wins = 0
+    def hand_value(self, hand=None):
+        As = 0 # Card counter "As"
+        hand = [self.hand[0]] if hand is not None else self.hand
+        self.num_hand.clear()
+
+        for card in hand:
+            if card == "As":
+                As += 1
+            else:
+                self.num_hand.append(card)
+        
+        for x in range(As): # 11 or 1 value for "As" (if there is)
+            if sum(self.num_hand) + 11 <= 21:
+                self.num_hand.append(11)
+            else:
+                self.num_hand.append(1)
+
+        self.score = sum(self.num_hand)
+        return self.score
+
+    def take_a_card(self):
+        self.hand.append(deck[0]) # Add a card to player hand
+        deck.pop(0) # Remove that card from deck
+        self.hand_value() # Recompute the hand value
 
 def dealing_cards():
     shuffle(deck)
 
     # Hands starts empty
-    player_hand.clear()
-    dealer_hand.clear()
-    phand.clear()
-    dhand.clear()
+    player1.hand.clear()
+    dealer.hand.clear()
+    player1.num_hand.clear()
+    dealer.num_hand.clear()
 
-    player_hand.extend(deck[0:2]) # Take the first two cards
+    player1.hand.extend(deck[0:2]) # Take the first two cards
     del deck[0:2] # Remove those cards taken from deck
 
-    dealer_hand.extend(deck[0:2])
+    dealer.hand.extend(deck[0:2])
     del deck[0:2]
 
-    global player_score, dealer_score
-    player_score = hand_value(player_hand, phand, player_score) # Compute the hand value
-    dealer_score = hand_value([dealer_hand[0]], dhand, dealer_score)
-
+    player1.hand_value() # Compute the hand value
     return
 
-def take_a_card(hand, pd_hand, score):
-    hand.append(deck[0]) # Add a card to (player/dealer) hand
-    deck.pop(0) # Remove that card from deck
-    score = hand_value(hand, pd_hand, score) # Recompute the hand value
+def show_some_cards():
+        return f"\n>> Dealer hand: [{dealer.hand[0]}, ?], the score is {dealer.hand_value(dealer.hand[0:])}\n" \
+                f">> {player1.name} hand: {player1.hand}, the score is {player1.score}\n"
 
-    return score
-
-def hand_value(hand, pd_hand, score):
-    As = 0 # Card counter "As"
-    pd_hand.clear()
-
-    for card in hand:
-        if card == "As":
-            As += 1
-        else:
-            pd_hand.append(card)
-    
-    for x in range(As): # 11 or 1 value for "As" (if there is)
-        if sum(pd_hand) + 11 <= 21:
-            pd_hand.append(11)
-        else:
-            pd_hand.append(1)
-
-    score = sum(pd_hand)
-    
-    return score
-
-def show_hands(dealer_hand):
-        return f"\n>> Dealer hand: {dealer_hand}, the score is {dealer_score}\n" \
-                f">> Your hand: {player_hand}, the score is {player_score}\n"
+def show_hole_cards():
+        return f"\n>> Dealer hand: {dealer.hand}, the score is {dealer.hand_value()}\n" \
+                f">> {player1.name} hand: {player1.hand}, the score is {player1.score}\n"
 
 def compare_scores():
-    global player_wins
-    if player_score > dealer_score:
-        player_wins += 1
+    if player1.score > dealer.score:
+        player1.wins += 1
         return "The player wins\n"
-    elif dealer_score > player_score:
+    elif dealer.score > player1.score:
         return "The dealer wins\n"
     else:
         return "It's a push\n"
 
 def play():
-    global player_score, dealer_score, player_wins
     blackjack = False
 
     print("\nThe dealer deals the cards...")
     dealing_cards()
-    print(show_hands([dealer_hand[0], "?"]))
-    
+    print(show_some_cards())
+
     # Evaluate if there is blackjack
-    hand_value(dealer_hand, dhand, dealer_score)
-    if dealer_score == 21:
+    dealer.score = dealer.hand_value()
+    if dealer.score == 21:
         blackjack = True
         return print(
-            f"Dealer hand: {dealer_hand}\n"
+            f"Dealer hand: {dealer.hand}\n"
             "The dealer gets BLACKJACK!!\n"
         )
-    elif player_score == 21:
+    elif player1.score == 21:
         blackjack = True
-        player_wins += 1
+        player1.wins += 1
         return print("The player gets BLACKJACK!!\n")
     
     # Player turn
@@ -104,22 +95,20 @@ def play():
 
         if hit_stand == "h":
             print("The player hits")
-            player_score = take_a_card(player_hand, phand, player_score)
-            dealer_score = hand_value([dealer_hand[0]], dhand, dealer_score)
-            print(show_hands([dealer_hand[0], "?"]))
-            if player_score > 21:
+            player1.take_a_card()
+            print(show_some_cards())
+            if player1.score > 21:
                 return print("The player busts\n")
         # Dealer turn
         elif hit_stand == "s":
             print("Dealer reveals the hole card...")
-            dealer_score = hand_value(dealer_hand, dhand, dealer_score)
-            print(show_hands(dealer_hand))
-            while dealer_score < 17:
+            print(show_hole_cards())
+            while dealer.score < 17:
                 print("The dealer draws a card")
-                dealer_score = take_a_card(dealer_hand, dhand, dealer_score)
-                print(show_hands(dealer_hand))
-                if dealer_score > 21:
-                    player_wins += 1
+                dealer.take_a_card()
+                print(show_hole_cards())
+                if dealer.score > 21:
+                    player1.wins += 1
                     return print("The dealer busts\n")
         else:
             print("Invalid input!\n")
@@ -139,6 +128,9 @@ def menu():
     )
 
 menu()
+
+player1 = Player("Bruno")
+dealer = Player("Dealer")
 
 option = 0
 while option != 1 or option != 2 or option != 3:
@@ -170,7 +162,7 @@ while option != 1 or option != 2 or option != 3:
             else:
                 print("Invalid input!\n")
     elif option == 2:
-        print(f"\n>> Total player wins: {player_wins}\n")
+        print(f"\n>> Total player wins: {player1.wins}\n")
     elif option == 3:
         break
     else:
